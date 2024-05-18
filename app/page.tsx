e15692e87d678bc132fbb198a90fceae26904f17
Michael Vargas <michael.devtloz@gmail.com>
2024-05-18 18:25:54 -0400

    gnail responsive

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default async function  Home() {
       </Fab>
       </Link>
       
-        <Link href={"https://mail.google.com/mail/?view=cm&fs=1&to=sierrafer.construdecor@gmail.com"} target="_blank">
+        <Link href={"mailto:sierrafer.construdecor@gmail.com"} target="_blank">
         <Fab
           color="primary"
           aria-label="add"
